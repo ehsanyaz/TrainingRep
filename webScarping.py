2d3c45f4c59a3d593ceeb16129a7d3d29252b3ac
Ehsan <yazdani.ehsan95@gmail.com>
2023-08-06 19:39:20 +0330

    Testing On Radok

--- webScarping.py
+++ webScarping.py
@@ -1,1 +1,14 @@
-print("Hi")
+import requests
+respone = requests.get("http://radostudio.com/")
+htmlTXT = respone.text
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(htmlTXT,"html.parser")
+persons = soup.find_all('h2', attrs = {'class' : 'elementor-heading-title elementor-size-default'})
+for name in persons:
+    print(name.text)
+
+
+#divs = soup.find_all("div")
+#for div in divs:
+#    print(div)
