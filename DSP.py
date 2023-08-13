3532673e298023debca02de0c43e71553b7d4a60
Ehsan <yazdani.ehsan95@gmail.com>
2023-08-13 23:14:14 +0330

    H3 Tag Adding

--- DSP.py
+++ DSP.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import asyncio
 from playwright.async_api import async_playwright
 
-async def run():
+async def func():
     async with async_playwright() as pw:
         browser = await pw.chromium.launch()
         page = await browser.new_page()
@@ -14,7 +14,7 @@ async def run():
         await page.goto(url)
         await page.wait_for_selector('h1')
         return url, await page.content()
-url , html = asyncio.run(run())
+url , html = asyncio.run(func())
 soup = BeautifulSoup(html,"html.parser")
-content = soup.find("h1")
+content = soup.find("h3", attrs = {"class" : "ellipsis-2 text-body2-strong color-700 styles_VerticalProductCard__productTitle__6zjjN"})
 print(content.text)
