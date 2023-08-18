e1e8a47fa996e3a53f14a0e55ec94daf0bc6b12b
Ehsan <yazdani.ehsan95@gmail.com>
2023-08-18 20:27:48 +0330

    WebScraping with Selenium

--- Selenium.py
+++ Selenium.py
@@ -4,24 +4,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 
 browser = webdriver.Firefox()  # start a web browser
-browser.get("https://en.wikipedia.org/wiki/Pok%C3%A9mon")  # navigate to URL
+browser.get("https://fa.wikipedia.org/wiki/%D8%A7%DB%8C%D8%B1%D8%A7%D9%86")  # navigate to URL
 # wait for page to load
 # by waiting for <h3> element to appear on the page
 WebDriverWait(driver=browser, timeout=10).until(visibility_of_element_located((By.CSS_SELECTOR, "h3")))
 
 # retrieve fully rendered HTML content
 content = browser.page_source
+
+# Title
 print("************")
-title = browser.find_elements(By.ID,"firstHeading")
-for t in title:
-    print(t.text)
+title = browser.find_element(By.ID, "firstHeading")
+print(title.text)
 print("************")
 
+# ScreenShot
 browser.save_screenshot("ScreenShot.png")
-'''links = browser.find_elements(By.TAG_NAME,"a")
+print("ScreenShot Taken")
+
+# links
+links = browser.find_elements(By.CLASS_NAME,"mw-redirect")
+print("************")
+print("Links")
+counter = 0
 for link in links:
-    print(link.get_attribute("href"))'''
-'''par = browser.find_elements(By.TAG_NAME,"p")
+    counter = counter + 1
+    print(counter, end="- \n")
+    print(link.text)
+    print(link.get_attribute("href"))
+print("************")
+
+
+# Content
+print("************")
+print("Content")
+par = browser.find_elements(By.TAG_NAME, "p")
 for p in par:
-    print(p.text)'''
+    print(p.text)
+print("************")
+
+
+
 browser.close()
