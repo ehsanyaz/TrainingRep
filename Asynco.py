66daa374ccde8b9bd41bbf1d84817fd0029170b9
Ehsan <yazdani.ehsan95@gmail.com>
2023-11-28 20:36:37 +0330

    Asynco Intro

---
+++ Asynco.py
@@ -0,0 +1,21 @@
+
+
+#!/usr/bin/env python3
+# countasync.py
+
+import asyncio
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+
+import time
+start_time = time.perf_counter()
+asyncio.run(main())
+elapsed = time.perf_counter() - start_time
+print(f"{__file__} executed in {elapsed:0.2f} seconds.")
